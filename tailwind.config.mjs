e7f4e0bf90b332a4650a5bc5eb34e51e2760b133
vegaswarrior <vegaswarrior888@gmail.com>
2024-03-28 18:50:21 -0700

    2nd commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend:  {
+			dropShadow: {
+				'3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+				'4xl': [
+					'0 35px 35px rgba(0, 0, 0, 0.25)',
+					'0 45px 65px rgba(0, 0, 0, 0.15)'
+				]
+			  },
+		}     
+		
+	
+	},
+	plugins: [],
+}
